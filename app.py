84390f06c52010a9224e24c21849d8404e548823
Stefanie-A <102748373+Stefanie-A@users.noreply.github.com>
2023-10-11 15:51:42 +0100

    initializing database

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-@app.route("/")
+@app.route("/home")
 def home():
-    return "CRUD APP"
+    return "Welcome"
 
 
-if __name__ == "__main_":
+if __name__ == "__main__":
     app.run(debug=True)
